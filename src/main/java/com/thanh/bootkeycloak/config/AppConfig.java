a77f1b3be094652311c60d7994e4b0fec8fd4b26
Nguyen Trung Thanh <thanhnt@nodo.vn>
2020-06-23 09:33:28 +0700

    Refactor code

--- src/main/java/com/thanh/bootkeycloak/config/AppConfig.java
+++ src/main/java/com/thanh/bootkeycloak/config/AppConfig.java
@@ -17,7 +17,6 @@ public class AppConfig {
         return localeResolver;
     }
 
-
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
